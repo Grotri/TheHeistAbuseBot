2795f368da5e065e43671cd74ee8f504bcd641b0
Grotri <progamer1205mail@gmail.com>
2023-07-04 12:08:09 +0300

    v1.0

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ from multiprocessing import Pool
 def setupDriver():
     options = Options()
     options.add_extension('phantom1.crx')
+    options.add_argument("--headless=new")
     options.add_argument("--window-size=1280,960")
+    options.page_load_strategy = 'eager'
+    options.add_argument("--no-sandbox")
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript': 2})
+    options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
     driver.get('https://google.com')
-    driver.switch_to.window(driver.window_handles[1])
     return driver
 
 
@@ -28,84 +33,86 @@ def findElement(xpath: str, driver):
 
 
 def getdata(words):
+    driver = setupDriver()
     try:
-        driver = setupDriver()
+        timer = 30
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
-        wallet_button = waitUntil(xpath='div/main/div[2]/div/div[2]/button[2]', time=3, driver=driver)
+        wallet_button = waitUntil(xpath='div/main/div[2]/div/div[2]/button[2]', time=timer, driver=driver)
         wallet_button.click()
         time.sleep(0.5)
 
-        word1 = findElement(xpath='div/main/div[2]/form/div/div[2]/div[1]/input', driver=driver)
+        word1 = waitUntil(xpath='div/main/div[2]/form/div/div[2]/div[1]/input', time=timer, driver=driver)
         word1.send_keys(words)
         time.sleep(0.3)
 
-        import_wallet = findElement(xpath='div/main/div[2]/form/button', driver=driver)
+        import_wallet = waitUntil(xpath='div/main/div[2]/form/button', time=timer, driver=driver)
         import_wallet.click()
-        import_check = waitUntil(xpath='div/main/div[2]/form/button[2]', time=5, driver=driver)
+        import_check = waitUntil(xpath='div/main/div[2]/form/button[2]', time=timer, driver=driver)
         import_check.click()
 
-        passw = waitUntil(xpath='div/main/div[2]/form/div[1]/div[2]/input', time=3, driver=driver)
+        passw = waitUntil(xpath='div/main/div[2]/form/div[1]/div[2]/input', time=timer, driver=driver)
         passw.send_keys('12345678')
 
-        pass_check = findElement(xpath='div/main/div[2]/form/div[1]/div[2]/div/div/input', driver=driver)
+        pass_check = waitUntil(xpath='div/main/div[2]/form/div[1]/div[2]/div/div/input', time=timer, driver=driver)
         pass_check.send_keys('12345678')
+        time.sleep(0.3)
 
         agree_check = findElement(xpath='div/main/div[2]/form/div[2]/span/input', driver=driver)
         agree_check.click()
         time.sleep(0.3)
 
-        continue_wallet = findElement(xpath='div/main/div[2]/form/button', driver=driver)
+        continue_wallet = waitUntil(xpath='div/main/div[2]/form/button', time=timer, driver=driver)
         continue_wallet.click()
         time.sleep(0.7)
 
-        final_wallet = findElement(xpath='div/main/div[2]/form/button', driver=driver)
+        final_wallet = waitUntil(xpath='div/main/div[2]/form/button', time=timer, driver=driver)
         final_wallet.click()
-        time.sleep(0.5)
-
-        finish_wallet = findElement(xpath='div/main/div[2]/form/button', driver=driver)
-        finish_wallet.click()
         time.sleep(1)
 
-        driver.switch_to.window(driver.window_handles[0])
         driver.get('https://theheist.game')
+        driver.switch_to.window(driver.window_handles[0])
 
-        connect = waitUntil(xpath='div/div[2]/header/div[3]/button', time=5, driver=driver)
+        connect = waitUntil(xpath='div/div[2]/header/div[3]/button', time=timer, driver=driver)
         connect.click()
 
-        connect_phantom = waitUntil(xpath='div/div[2]/header/div[3]/ul/li[1]/button', time=3, driver=driver)
+        connect_phantom = waitUntil(xpath='div/div[2]/header/div[3]/ul/li[1]/button', time=timer, driver=driver)
         connect_phantom.click()
         time.sleep(1)
 
         driver.switch_to.window(driver.window_handles[1])
 
-        connect_in_phantom = waitUntil(xpath='div/div/div[1]/div[2]/div/button[2]', time=3, driver=driver)
+        connect_in_phantom = waitUntil(xpath='div/div/div[1]/div[2]/div/button[2]', time=timer, driver=driver)
         connect_in_phantom.click()
         time.sleep(3)
 
         driver.switch_to.window(driver.window_handles[1])
 
-        approve = waitUntil(xpath='div/div/div[1]/div/div[2]/div/button[2]', time=3, driver=driver)
+        approve = waitUntil(xpath='div/div/div[1]/div/div[2]/div/button[2]', time=timer, driver=driver)
         approve.click()
         time.sleep(2)
 
         driver.switch_to.window(driver.window_handles[0])
 
-        hub_button = waitUntil(xpath='div/div[2]/div[3]/div/div[1]/div[2]/button[1]', time=5, driver=driver)
+        hub_button = waitUntil(xpath='div/div[2]/div[3]/div/div[1]/div[2]/button[1]', time=timer, driver=driver)
         hub_button.click()
         time.sleep(1)
 
-        rewards_button = waitUntil(xpath='div[2]/div[3]/div/div[1]/div/div[7]/button', time=30, driver=driver)
+        rewards_button = waitUntil(xpath='div[2]/div[3]/div/div[1]/div/div[7]/button', time=timer, driver=driver)
         rewards_button.click()
         time.sleep(3)
 
-        # claim_ticket = waitUntil(xpath='div[2]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/button', time=30, driver=driver)
-        # claim_ticket.click()
-        # time.sleep(3)
-
+        claim_ticket = waitUntil(xpath='div[2]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/button', time=timer, driver=driver)
+        claim_ticket.click()
         time.sleep(3)
-        log = open('log.txt', mode='a')
-        log.write(words + '\n')
-        log.close()
+        print('Ticket claimed successfully!')
+
+        throw_ticket = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[3]/div/div[2]/div[2]/div[1]/div[1]/div[4]/button[2]')))
+        throw_ticket.click()
+        time.sleep(3)
+
+        9
     except Exception as ex:
         print(ex)
     finally:
@@ -117,10 +124,11 @@ if __name__ == '__main__':
     total_tickets = int(input("Enter number of total tickets to process: "))
     threads_num = int(input("Enter number of treads: "))
     mnems = []
-    for i in range(1, math.ceil(total_tickets / threads_num)):
-        for n in range(1, threads_num):
+    for i in range(0, math.ceil(total_tickets / threads_num)):
+        for n in range(0, threads_num):
             mnemo = Mnemonic("english")
             words = mnemo.generate(strength=128)
             mnems.append(words)
         p = Pool(processes=threads_num)
         p.map(getdata, mnems)
+        mnems = []
